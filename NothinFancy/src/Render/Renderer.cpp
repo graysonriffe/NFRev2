201469570697005cf0d7e87742782980722738f8
Grayson Riffe <grayson@graysonriffe.com>
2023-11-12 11:20:44 -0600

    First triangle

--- NothinFancy/src/Render/Renderer.cpp
+++ NothinFancy/src/Render/Renderer.cpp
@@ -4,6 +4,10 @@
 #include "nf/Utility/Util.h"
 
 namespace nf::render {
+	static const float s_black[] = {
+		0.0f, 0.0f, 0.0f, 1.0f
+	};
+
 	Renderer::Renderer(Window& window, DisplayConfig& dispConfig)
 		: m_window(window)
 	{
@@ -60,6 +64,44 @@ namespace nf::render {
 		dxgiFactory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_WINDOW_CHANGES);
 
 		setDisplay(dispConfig);
+
+		float triangle[] = {
+			-0.5f, -0.5f,
+			0.0f, 0.5f,
+			0.5f, -0.5f
+		};
+
+		D3D11_BUFFER_DESC buffDesc = {};
+		buffDesc.ByteWidth = sizeof(triangle);
+		buffDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+		D3D11_SUBRESOURCE_DATA sub = {};
+		sub.pSysMem = triangle;
+
+		m_device->CreateBuffer(&buffDesc, &sub, m_testBuffer.GetAddressOf());
+
+		unsigned int stride = 8, offset = 0;
+		m_context->IASetVertexBuffers(0, 1, m_testBuffer.GetAddressOf(), &stride, &offset);
+
+		std::string vertexShader, pixelShader;
+		util::readFile("BasicVertex.cso", vertexShader);
+		util::readFile("BasicPixel.cso", pixelShader);
+
+		m_device->CreateVertexShader(vertexShader.c_str(), vertexShader.size(), nullptr, m_testVertexShader.GetAddressOf());
+		m_context->VSSetShader(m_testVertexShader.Get(), nullptr, 0);
+		m_device->CreatePixelShader(pixelShader.c_str(), pixelShader.size(), nullptr, m_testPixelShader.GetAddressOf());
+		m_context->PSSetShader(m_testPixelShader.Get(), nullptr, 0);
+
+		D3D11_INPUT_ELEMENT_DESC elemDesc = {};
+		elemDesc.SemanticName = "POSITION";
+		elemDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
+		elemDesc.AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+		elemDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+
+		m_device->CreateInputLayout(&elemDesc, 1, vertexShader.c_str(), vertexShader.size(), m_testLayout.GetAddressOf());
+		m_context->IASetInputLayout(m_testLayout.Get());
+
+		m_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 	}
 
 	void Renderer::setDisplay(DisplayConfig& conf) {
@@ -83,9 +125,33 @@ namespace nf::render {
 		}
 
 		m_sc->ResizeTarget(&scDesc);
+
+		m_context->OMSetRenderTargets(0, nullptr, nullptr);
+		if (m_outRTV)
+			m_outRTV->Release();
+
+		m_sc->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, NULL);
+		ComPtr<ID3D11Texture2D> backBuff;
+		m_sc->GetBuffer(0, __uuidof(backBuff), reinterpret_cast<void**>(backBuff.GetAddressOf()));
+		D3D11_RENDER_TARGET_VIEW_DESC1 rtvDesc = {};
+		rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+		rtvDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+		m_device->CreateRenderTargetView1(backBuff.Get(), &rtvDesc, m_outRTV.GetAddressOf());
+
+		D3D11_VIEWPORT vp = {};
+		vp.Width = static_cast<float>(scDesc.Width);
+		vp.Height = static_cast<float>(scDesc.Height);
+		vp.MinDepth = 0.0f;
+		vp.MaxDepth = 1.0f;
+		m_context->RSSetViewports(1, &vp);
 	}
 
 	void Renderer::doFrame() {
+		m_context->ClearRenderTargetView(m_outRTV.Get(), s_black);
+		m_context->OMSetRenderTargets(1, reinterpret_cast<ID3D11RenderTargetView**>(m_outRTV.GetAddressOf()), nullptr);
+
+		m_context->Draw(3, 0);
+
 		m_sc->Present(0, NULL);
 	}
 
--- NothinFancy/src/Render/Renderer.h
+++ NothinFancy/src/Render/Renderer.h
@@ -21,5 +21,12 @@ namespace nf::render {
 		ComPtr<ID3D11Device3> m_device;
 		ComPtr<ID3D11DeviceContext4> m_context;
 		ComPtr<IDXGISwapChain2> m_sc;
+		ComPtr<ID3D11RenderTargetView1> m_outRTV;
+
+		//Test stuff
+		ComPtr<ID3D11Buffer> m_testBuffer;
+		ComPtr<ID3D11InputLayout> m_testLayout;
+		ComPtr<ID3D11VertexShader> m_testVertexShader;
+		ComPtr<ID3D11PixelShader> m_testPixelShader;
 	};
 }
