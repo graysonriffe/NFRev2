fceda8c4a2c16b0c7c4b1ec3306b61a306a1ebde
Grayson Riffe <grayson@graysonriffe.com>
2023-11-29 20:19:33 -0600

    Change asset builder script message

--- NFAssetBuilder/scripts/PreBuild.bat
+++ NFAssetBuilder/scripts/PreBuild.bat
@@ -4,8 +4,8 @@ echo [NF Asset Builder Pre Build] Startup
 
 set workingDir=%1
 
+echo [NF Asset Builder Pre Build] Checking working directory...
 if exist %workingDir% goto continue
-echo [NF Asset Builder Pre Build] Creating working directory...
 mkdir %workingDir%
 
 :continue
